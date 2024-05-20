3653c1581e27598b1eb9be23416397df1470fc1a
Marc Micatka <micat001@uw.edu>
2024-05-20 12:52:53 -0700

    yo wtf but this is working on py11

--- output/setup.py
+++ output/setup.py
@@ -1,18 +1,31 @@
+from distutils.core import Extension, setup
+
 import setuptools
-from distutils.core import setup, Extension
+
+extra_objects = [
+    "/usr/lib/x86_64-linux-gnu/lapack/liblapack.a",
+    "/usr/lib/x86_64-linux-gnu/libgfortran.so.5.0.0",
+    "/usr/lib/x86_64-linux-gnu/blas/libblas.a",
+]
 
 
 def main():
     setup(
-        name='pyikfast_bravo',
-        version='0.0.1',
-        description='ikfast wrapper',
-        author='Cyberbotics',
-        author_email='support@cyberbotics.com',
-        ext_modules=[Extension('pyikfast_bravo', ['ikfast_robot.cpp', 'pyikfast.cpp'])],
-        setup_requires=['wheel']
+        name="pyikfast_bravo7",
+        version="0.0.1",
+        description="ikfast wrapper",
+        author="Cyberbotics",
+        author_email="support@cyberbotics.com",
+        ext_modules=[
+            Extension(
+                "pyikfast_bravo7",
+                ["ikfast_robot.cpp", "pyikfast.cpp"],
+                extra_objects=extra_objects,
+            )
+        ],
+        setup_requires=["wheel"],
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
